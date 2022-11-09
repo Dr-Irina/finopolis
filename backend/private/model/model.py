b743b72d29c8c28a48ea0a096bb4bfa1a82ca87f
andrw.shibaev <andrw.shibaev>
2022-11-09 23:20:39 +0100

    cached models

--- backend/private/model/model.py
+++ backend/private/model/model.py
@@ -1,23 +1,24 @@
 from collections import namedtuple
+from functools import lru_cache
+
 import torch
 from transformers import AutoModelForSequenceClassification
 from transformers import BertTokenizerFast
 
-tokenizer = None
-model = None
 
-
+@lru_cache
 def init_models():
-    tokenizer = BertTokenizerFast.from_pretrained('blanchefort/rubert-base-cased-sentiment')
-    model = AutoModelForSequenceClassification.from_pretrained('blanchefort/rubert-base-cased-sentiment', return_dict=True)
+    return BertTokenizerFast.from_pretrained(
+        'blanchefort/rubert-base-cased-sentiment'
+    ), AutoModelForSequenceClassification.from_pretrained('blanchefort/rubert-base-cased-sentiment', return_dict=True)
+
 
 PredictResult = namedtuple('PredictResult', ['neutral', 'negative', 'positive'])
 
 
 @torch.no_grad()
 def predict(text):
-    if tokenizer is None or model is None:
-        init_models()
+    tokenizer, model = init_models()
     inputs = tokenizer(text, max_length=512, padding=True, truncation=True, return_tensors='pt')
     outputs = model(**inputs)
     predicted = torch.nn.functional.softmax(outputs.logits, dim=1)[0]
